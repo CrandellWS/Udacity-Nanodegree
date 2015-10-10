4e43aa223e072ee127aa9fd7995e06f9e1ad0e73
William Crandell <dev@crandell.ws>
2015-10-10 12:39:47 -0400

    rename files p1

---
+++ Programming Foundations with Python/Lesson 1/rename_files.py
@@ -0,0 +1,10 @@
+import os
+
+def rename_files():
+  # get file in folder
+  file_list = os.listdir("C:\Users\William\Projects\python\Udacity - Non Github\prank")
+  print(file_list)
+  
+  # for each file in list rename it
+  
+rename_files()
