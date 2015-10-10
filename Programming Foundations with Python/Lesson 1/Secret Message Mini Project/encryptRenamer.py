de2a061a675f29d88a4de5f45a94af42548e7305
William Crandell <dev@crandell.ws>
2015-10-10 14:44:21 -0400

    Ok first run at encryption

--- Programming Foundations with Python/Lesson 1/Secret Message Mini Project/encryptRenamer.py
+++ Programming Foundations with Python/Lesson 1/Secret Message Mini Project/encryptRenamer.py
@@ -21,27 +21,8 @@ def encryptDirFileNames(parent_directory):
      while i < nLetters:
        fLetters = fLetters + random.choice(string.ascii_uppercase)
        i = i + 1 
-     print(fLetters + file_name)
-##def removeNumbersFromFilesInDir(search_directory):
-##  # get file in folder
-##
-##  file_list = os.listdir(search_directory)
-##  print(file_list + file_name)
+     os.rename(file_name, fLetters + file_name)
 
-##  saved_path = os.getcwd()
-##  print("Current Working Directory is " + saved_path)
-##
-##  os.chdir(search_directory)
-  
-  
-## # for each file in list rename it
-##  for file_name in file_list:
-##    print("Old Filename: " + file_name)
-##    os.rename(search_directory+os.sep+ file_name, search_directory+os.sep+ file_name.translate(None, "0123456789"))
-##    print("New Filename: " +os.sep+  file_name.translate(None, "0123456789"))
-
-##  os.chdir(saved_path)
-    
 search_directory = "C:\Users\William\Projects\python\Udacity\Programming Foundations with Python\Lesson 1\Secret Message Mini Project"
 
 encryptDirFileNames(search_directory)
