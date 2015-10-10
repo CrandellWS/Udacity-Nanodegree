ba9b664d1db145268f6178530149a1e62cb0c982
William Crandell <dev@crandell.ws>
2015-10-10 12:48:33 -0400

    rename files p2

--- Programming Foundations with Python/Lesson 1/rename_files.py
+++ Programming Foundations with Python/Lesson 1/rename_files.py
@@ -6,5 +6,7 @@ def rename_files():
   print(file_list)
   
   # for each file in list rename it
-  
+  for file_name in file_list:
+    os.rename(file_name, file_name.translate(None, "0123456789"))
+    
 rename_files()
