f447d919d63717a54eebe56ade3b5123ebce0987
William Crandell <dev@crandell.ws>
2015-10-10 12:59:21 -0400

    rename files p4

--- Programming Foundations with Python/Lesson 1/rename_files.py
+++ Programming Foundations with Python/Lesson 1/rename_files.py
@@ -1,15 +1,20 @@
 import os
 
 def rename_files():
-##  # get file in folder
-##  search_directory = "C:\Users\William\Projects\python\Udacity - Non Github\prank"
-##  file_list = os.listdir(search_directory)
-##  print(file_list)
+  # get file in folder
+  search_directory = "C:\Users\William\Projects\python\Udacity - Non Github\prank"
+  file_list = os.listdir(search_directory)
+  print(file_list)
 
   saved_path = os.getcwd()
   print("Current Working Directory is " + saved_path)
-##  # for each file in list rename it
-##  for file_name in file_list:
-##    os.rename(file_name, search _directory+file_name.translate(None, "0123456789"))
-##    
+
+  os.chdir(search_directory)
+  
+  
+  # for each file in list rename it
+  for file_name in file_list:
+    os.rename(file_name, search _directory+file_name.translate(None, "0123456789"))
+
+  os.chdir(saved_path)
 rename_files()
