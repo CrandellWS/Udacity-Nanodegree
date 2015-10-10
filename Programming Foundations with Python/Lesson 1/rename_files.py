374cd5db47442028efdffbaa7d4556e4e930ac83
William Crandell <dev@crandell.ws>
2015-10-10 13:20:29 -0400

    rename files my quick small changes

--- Programming Foundations with Python/Lesson 1/rename_files.py
+++ Programming Foundations with Python/Lesson 1/rename_files.py
@@ -1,20 +1,25 @@
 import os
 
-def rename_files():
+def removeNumbersFromFilesInDir(search_directory):
   # get file in folder
-  search_directory = "C:\Users\William\Projects\python\Udacity - Non Github\prank"
+
   file_list = os.listdir(search_directory)
   print(file_list)
 
-  saved_path = os.getcwd()
-  print("Current Working Directory is " + saved_path)
-
-  os.chdir(search_directory)
+##  saved_path = os.getcwd()
+##  print("Current Working Directory is " + saved_path)
+##
+##  os.chdir(search_directory)
   
   
   # for each file in list rename it
   for file_name in file_list:
-    os.rename(file_name, search_directory+file_name.translate(None, "0123456789"))
+    print("Old Filename: " + file_name)
+    os.rename(search_directory+os.sep+ file_name, search_directory+os.sep+ file_name.translate(None, "0123456789"))
+    print("New Filename: " +os.sep+  file_name.translate(None, "0123456789"))
 
-  os.chdir(saved_path)
-rename_files()
+##  os.chdir(saved_path)
+    
+search_directory = "C:\Users\William\Projects\python\Udacity - Non Github\prank"
+
+removeNumbersFromFilesInDir(search_directory)
