302e187ec63f26425215cd2837ea6d39552cf356
William Crandell <dev@crandell.ws>
2015-10-10 21:33:04 -0400

    Ok so it helps that window was used where the docs use screen as a variable. Caught me slipping

--- Programming Foundations with Python/Lesson 2/mindstorms.py
+++ Programming Foundations with Python/Lesson 2/mindstorms.py
@@ -1,15 +1,21 @@
 import turtle
 
 def draw_square():
+  ## window is my screen
   window = turtle.Screen()
   window.bgcolor("green")
   i = 0
   Parker = turtle.Turtle()
-  while(i < 4):
-    i = i + 1
+  ## no there is not a screen import it is a function in turtle...
+  window.register_shape("parker.gif")
+  Parker.shape("parker.gif")
+
+  i = 0
+  while(i< 4):
     ## parker's my dog but now here Parker listens. 
     Parker.forward(100)
     Parker.right(90)
+    i = i + 1
   
   window.exitonclick()
 
