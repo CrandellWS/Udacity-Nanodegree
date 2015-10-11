ca136f1dc83af03058dd89c7a1373a55dcb08aff
William Crandell <dev@crandell.ws>
2015-10-10 20:53:04 -0400

    I used a while loop -> still struggling with python indents

--- Programming Foundations with Python/Lesson 2/mindstorms.py
+++ Programming Foundations with Python/Lesson 2/mindstorms.py
@@ -1,13 +1,16 @@
 import turtle
 
 def draw_square():
-    window = turtle.Screen()
-    window.bgcolor("green")
-
-    Parker = turtle.Turtle()
+  window = turtle.Screen()
+  window.bgcolor("green")
+  i = 0
+  Parker = turtle.Turtle()
+  while(i < 4):
+    i = i + 1
     ## parker's my dog but now here Parker listens. 
     Parker.forward(100)
-
-    window.exitonclick()
+    Parker.right(90)
+  
+  window.exitonclick()
 
 draw_square()  
