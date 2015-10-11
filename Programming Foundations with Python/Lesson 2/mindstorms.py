0ce4382e1bfa903bc2daae2a7eb5eafa45e73f41
William Crandell <dev@crandell.ws>
2015-10-11 00:05:27 -0400

    I should work on my math

--- Programming Foundations with Python/Lesson 2/mindstorms.py
+++ Programming Foundations with Python/Lesson 2/mindstorms.py
@@ -1,10 +1,10 @@
 import turtle
 
-def draw_square():
-  ## window is my screen
-  window = turtle.Screen()
-  window.bgcolor("green")
-  i = 0
+## window is my screen
+window = turtle.Screen()
+window.bgcolor("green")
+
+def parker_draw_square():
   Parker = turtle.Turtle()
   ## no there is not a screen import it is a function in turtle...
   window.register_shape("Parker.gif")
@@ -18,18 +18,48 @@ def draw_square():
     Parker.forward(100)
     i = i + 1
   
-  Parker.penup()
-  Parker.right(90)
-  Parker.forward(100)
+  Parker.up()
+  Parker.forward(200)
   Parker.fill(False)
   
+def lucy_draw_circle():
   Lucy = turtle.Turtle()
   window.register_shape("Lucy.gif")
   Lucy.shape("Lucy.gif")
+  Lucy.up()
+  Lucy.right(45)
+  Lucy.forward(100)
+  Lucy.down()
   Lucy.pencolor("red")
   Lucy.circle(100)
-
-  ##had to move below Lucy to get Lucy to work
-  window.exitonclick()
+  Lucy.left(45)
+  Lucy.up()
+  Lucy.forward(66)
   
-draw_square()  
+def sugar_draw_triangle():
+  Sugar = turtle.Turtle()
+  window.register_shape("Sugar.gif")
+  Sugar.up()
+  Sugar.left(90)
+  Sugar.forward(100)
+  Sugar.down()
+  Sugar.shape("Sugar.gif")
+  Sugar.pencolor("orange")
+  i =0 
+  while(i < 2):
+    Sugar.left(60)
+    Sugar.forward(58)
+    i = i + 1
+  Sugar.left(180 -30.45)
+  Sugar.forward(100)
+  Sugar.up()
+  Sugar.right(45)
+  Sugar.forward(133)
+  
+def start_drawing():  
+  parker_draw_square()
+  sugar_draw_triangle()
+  lucy_draw_circle()
+
+start_drawing()
+window.exitonclick()
