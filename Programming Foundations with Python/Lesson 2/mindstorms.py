e4c3be1cdf6ac0d1775039494aaa730f9a198bf7
William Crandell <dev@crandell.ws>
2015-10-11 01:58:52 -0400

    putting code back in place

--- Programming Foundations with Python/Lesson 2/mindstorms.py
+++ Programming Foundations with Python/Lesson 2/mindstorms.py
@@ -16,6 +16,41 @@ def draw_square(value_input):
     value_input.forward(100)
     i = i + 1
   
+def draw_circle(value_input):
+  value_string = str(value_input)
+  value_input = turtle.Turtle()
+  window.register_shape(value_string+".gif")
+  value_input.shape(value_string+".gif")
+  value_input.up()
+  value_input.right(45)
+  value_input.forward(100)
+  value_input.down()
+  value_input.pencolor("red")
+  value_input.circle(100)
+  value_input.left(45)
+  value_input.up()
+  value_input.forward(66)
+  
+def draw_triangle(value_input):
+  value_string = str(value_input)
+  value_input = turtle.Turtle()
+  window.register_shape(value_string+".gif")
+  value_input.up()
+  value_input.left(90)
+  value_input.forward(100)
+  value_input.down()
+  value_input.shape(value_string+".gif")
+  value_input.pencolor("orange")
+  i =0 
+  while(i < 2):
+    value_input.left(60)
+    value_input.forward(58)
+    i = i + 1
+  value_input.left(180 -30.45)
+  value_input.forward(100)
+  value_input.up()
+  value_input.right(45)
+  value_input.forward(133)
 
 def start_drawing(value_input):
   the_turtle = init_turtle(value_input)
@@ -23,6 +58,10 @@ def start_drawing(value_input):
     print(n)
     draw_square(the_turtle)
     the_turtle.left(10)
+  
+##  draw_triangle("Sugar")
+##  draw_circle("Lucy")
+
 
 ## window is my screen
 window = turtle.Screen()
