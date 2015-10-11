32b6558e4e7f8c5c2674586152eab8f4e6b36eed
William Crandell <dev@crandell.ws>
2015-10-11 01:28:42 -0400

    using functions with inputs seems that the window needs to stay in scope or be global

--- Programming Foundations with Python/Lesson 2/mindstorms.py
+++ Programming Foundations with Python/Lesson 2/mindstorms.py
@@ -1,65 +1,73 @@
 import turtle
 
-## window is my screen
-window = turtle.Screen()
-window.bgcolor("green")
 
-def parker_draw_square():
-  Parker = turtle.Turtle()
+def draw_square(value_input):
+  value_string = str(value_input)
+  value_input = turtle.Turtle()
   ## no there is not a screen import it is a function in turtle...
-  window.register_shape("Parker.gif")
-  Parker.shape("Parker.gif")
-  Parker.pencolor("brown")
-  Parker.fill(True)
+  print(value_string)
+  print(value_string+".gif")
+  window.register_shape(value_string+".gif")
+  value_input.shape(value_string+".gif")
+  value_input.pencolor("brown")
+  value_input.fill(True)
   i = 0
   while(i< 4):
     ## parker's my dog but now here Parker listens. 
-    Parker.left(90)
-    Parker.forward(100)
+    value_input.left(90)
+    value_input.forward(100)
     i = i + 1
   
-  Parker.up()
-  Parker.forward(200)
-  Parker.fill(False)
+  value_input.fill(False)
+  value_input.up()
+  value_input.forward(200)
+
   
-def lucy_draw_circle():
-  Lucy = turtle.Turtle()
-  window.register_shape("Lucy.gif")
-  Lucy.shape("Lucy.gif")
-  Lucy.up()
-  Lucy.right(45)
-  Lucy.forward(100)
-  Lucy.down()
-  Lucy.pencolor("red")
-  Lucy.circle(100)
-  Lucy.left(45)
-  Lucy.up()
-  Lucy.forward(66)
+def draw_circle(value_input):
+  value_string = str(value_input)
+  value_input = turtle.Turtle()
+  window.register_shape(value_string+".gif")
+  value_input.shape(value_string+".gif")
+  value_input.up()
+  value_input.right(45)
+  value_input.forward(100)
+  value_input.down()
+  value_input.pencolor("red")
+  value_input.circle(100)
+  value_input.left(45)
+  value_input.up()
+  value_input.forward(66)
+
   
-def sugar_draw_triangle():
-  Sugar = turtle.Turtle()
-  window.register_shape("Sugar.gif")
-  Sugar.up()
-  Sugar.left(90)
-  Sugar.forward(100)
-  Sugar.down()
-  Sugar.shape("Sugar.gif")
-  Sugar.pencolor("orange")
+def draw_triangle(value_input):
+  value_string = str(value_input)
+  value_input = turtle.Turtle()
+  window.register_shape(value_string+".gif")
+  value_input.up()
+  value_input.left(90)
+  value_input.forward(100)
+  value_input.down()
+  value_input.shape(value_string+".gif")
+  value_input.pencolor("orange")
   i =0 
   while(i < 2):
-    Sugar.left(60)
-    Sugar.forward(58)
+    value_input.left(60)
+    value_input.forward(58)
     i = i + 1
-  Sugar.left(180 -30.45)
-  Sugar.forward(100)
-  Sugar.up()
-  Sugar.right(45)
-  Sugar.forward(133)
+  value_input.left(180 -30.45)
+  value_input.forward(100)
+  value_input.up()
+  value_input.right(45)
+  value_input.forward(133)
   
-def start_drawing():  
-  parker_draw_square()
-  sugar_draw_triangle()
-  lucy_draw_circle()
+def start_drawing():
+  draw_square("Parker")
+  draw_triangle("Sugar")
+  draw_circle("Lucy")
 
+
+## window is my screen
+window = turtle.Screen()
+window.bgcolor("green")
 start_drawing()
 window.exitonclick()
