eeaf4145ea8a0710197ffab0fb6df1a928b6f19c
William Crandell <dev@crandell.ws>
2015-10-10 20:48:39 -0400

    Draw a line Parker

---
+++ Programming Foundations with Python/Lesson 2/mindstorms.py
@@ -0,0 +1,13 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("green")
+
+    Parker = turtle.Turtle()
+    ## parker's my dog but now here Parker listens. 
+    Parker.forward(100)
+
+    window.exitonclick()
+
+draw_square()  
