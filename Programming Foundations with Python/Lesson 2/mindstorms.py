d1445f52cba7efed2ba1e6de574c931bc450abfe
William Crandell <dev@crandell.ws>
2015-10-10 23:22:55 -0400

    progress update

--- Programming Foundations with Python/Lesson 2/mindstorms.py
+++ Programming Foundations with Python/Lesson 2/mindstorms.py
@@ -7,8 +7,8 @@ def draw_square():
   i = 0
   Parker = turtle.Turtle()
   ## no there is not a screen import it is a function in turtle...
-  window.register_shape("parker.gif")
-  Parker.shape("parker.gif")
+  window.register_shape("Parker.gif")
+  Parker.shape("Parker.gif")
   Parker.pencolor("brown")
   Parker.fill(True)
   i = 0
@@ -22,6 +22,14 @@ def draw_square():
   Parker.right(90)
   Parker.forward(100)
   Parker.fill(False)
-  window.exitonclick()
+  
+  Lucy = turtle.Turtle()
+  window.register_shape("Lucy.gif")
+  Lucy.shape("Lucy.gif")
+  Lucy.pencolor("red")
+  Lucy.circle(100)
 
+  ##had to move below Lucy to get Lucy to work
+  window.exitonclick()
+  
 draw_square()  
