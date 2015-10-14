bda5e0d2007eb60d739a61267af78a85dd61c1d6
William Crandell <william@crandell.ws>
2015-10-14 05:09:08 -0400

    had my fun with turtle
      Time to continue on

--- Programming Foundations with Python/Lesson 2/2b-miniproject-take2.py
+++ Programming Foundations with Python/Lesson 2/2b-miniproject-take2.py
@@ -4,7 +4,7 @@ import time
 ## Take 2
 
 def w_for_william(the_turtle,depth, size):
-  if depth < 2:
+  if depth == 1:
     the_turtle.forward(size)
   else:
     recurse = lambda: w_for_william(the_turtle,depth-1, size/6)
@@ -24,46 +24,68 @@ def init_turtle(value_input):
   value_string = str(value_input)
   value_input = turtle.Turtle()
   value_input.shape("turtle")
-  value_input.pencolor("green")
+  value_input.pencolor("black")
   value_input.speed(0)
   return value_input
   
 def try_it(value_input, sides, length):
   offset = 360 / sides
   the_turtle = init_turtle(value_input)
-  the_turtle.fillcolor("orange")
+  the_turtle.fillcolor("black")
+  the_turtle.fillcolor("green")
+  the_turtle.up()
+  the_turtle.goto(-20, -300)
+  the_turtle.down()
+  the_turtle.begin_fill()
+  the_turtle.pensize(3)
+  the_turtle.circle(300)
+  the_turtle.end_fill()
+  the_turtle.up()
+  the_turtle.goto(20, -265)
+  the_turtle.fillcolor("#FF6700")
+  the_turtle.begin_fill()
+  the_turtle.circle(265)
+  the_turtle.end_fill()
+  the_turtle.up()
+  the_turtle.circle(265, 60)
+  the_turtle.down()
+  the_turtle.circle(265, -300)
+  the_turtle.up()
+  the_turtle.goto(100, 60)
+  the_turtle.fillcolor("black")
+  the_turtle.begin_fill()
+  the_turtle.circle(120)
+  the_turtle.end_fill()
+  the_turtle.goto(0, 0)
+  the_turtle.down()
+  the_turtle.fillcolor("#EED202")
+  the_turtle.pencolor("green")
   i = 0
+  print(the_turtle.pensize())
+  the_turtle.pensize(1)
   while(i < sides):
-    print("i", i)
     the_turtle.begin_fill()
-    i = i + 1
-    w_for_william(the_turtle, 3 , length)
-    the_turtle.right(360/sides)
     n = 0
     while(n < sides):
-      print("n", n)
       n = n + 1
       w_for_william(the_turtle, 3 , length * 2)
       the_turtle.right(360/sides*2)
       m = 0
       while(m < sides):
-        print("m", m)
         m = m + 1
         w_for_william(the_turtle, 3 , length * 2*3)
         the_turtle.right(360/sides*2*3)
+    i = i + 1
+    w_for_william(the_turtle, 3 , length)
+    the_turtle.right(360/sides)
     the_turtle.end_fill()
     
-  the_turtle.fillcolor("black")
   the_turtle.goto(0, -50)
-  the_turtle.up()
-  the_turtle.fill(True)
-  the_turtle.circle(50)
-  the_turtle.fill(False)
-  the_turtle.down()
-  the_turtle.ht()
+  the_turtle.fillcolor("green")
+  the_turtle.pencolor("black")
     
 ## window is my screen
 window = turtle.Screen()
-window.bgcolor("black")
+window.bgcolor("#FF6700")
 try_it("Sugar", 3, 3**3)
 window.exitonclick()
