c8ccfa3fa4f72afb4ceb2da933e7c0c5529cb614
William Crandell <william@crandell.ws>
2015-10-14 11:03:39 -0400

    Created Twilio Auth Class
      The file SAMPLE__twilioauth.py is to show how the external auth
      data is delivered. It is just a very basic class. It required
      The contianing folder to have a __init__.py to be able to be imported

---
+++ Programming Foundations with Python/Lesson 2/2b/SAMPLE_twilioauth.py
@@ -0,0 +1,7 @@
+class TwilioAuth:
+  def __init__(self):    
+    self.account_sid = "XxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXx"
+    self.auth_token  = "XxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXx"
+    self.to = "+19871234567"
+    self.from_ = "+19871234567"
+    
--- Programming Foundations with Python/Lesson 2/2b/send_text.py
+++ Programming Foundations with Python/Lesson 2/2b/send_text.py
@@ -1,11 +1,21 @@
+## needed to add directory to path
+import os.path, sys
+## adding directory outside of repository to path
+sys.path.append("C:\Users\William\Projects\python\Udacity - Non Github\AuthTokens")
+## to import the real auth data
+import twilioauth
+
+
 from twilio.rest import TwilioRestClient
- 
+auth_data = twilioauth.TwilioAuth()
+
+print(auth_data.to)
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "XXX"
-auth_token  = "XXX"
+account_sid = auth_data.account_sid
+auth_token  = auth_data.auth_token
 client = TwilioRestClient(account_sid, auth_token)
  
-message = client.messages.create(body="Ok then.",
-    to="+XXX",    # Replace with your phone number
-    from_="+XXX") # Replace with your Twilio number
+message = client.messages.create(body="Morning Mom, Are you going to be able to give me a ride tomorrow? I can ride the bus.",
+    to=auth_data.to,    # Replace with your phone number
+    from_=auth_data.from_) # Replace with your Twilio number
 print message.sid
