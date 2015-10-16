6e50e87c23702ba7fba7bbbd16e9ea75bb95573f
William Crandell <william@crandell.ws>
2015-10-15 20:08:07 -0400

    Lesson 3 following along

--- Programming Foundations with Python/Lesson 3/movies/entertainment_center.py
+++ Programming Foundations with Python/Lesson 3/movies/entertainment_center.py
@@ -1,4 +1,10 @@
 import media
 
+title = "Toy Story"
+storyline = "A story of a boy and his toys that come to life"
+poster_image_url = "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg"
+trailer_youtube_url = "https://www.youtube.com/watch?v=vwyZH85NQC4"
 
-toy_story = media.Movie()
+toy_story = media.Movie(title, storyline, poster_image_url, trailer_youtube_url)
+
+print(toy_story.storyline)
--- Programming Foundations with Python/Lesson 3/movies/media.py
+++ Programming Foundations with Python/Lesson 3/movies/media.py
@@ -1,1 +1,6 @@
 class Movie():
+    def __init__(self, title_, storyline_, poster_image_url_, trailer_youtube_url_):
+        self.title = title_
+        self.storyline = storyline_
+        self.poster_image_url = poster_image_url_
+        self.trailer_youtube_url = trailer_youtube_url_
