06da0bdb4452061adcb8ee9c93475575b8aabf47
William Crandell <william@crandell.ws>
2015-10-15 20:16:50 -0400

    updating with lesson

--- Programming Foundations with Python/Lesson 3/movies/entertainment_center.py
+++ Programming Foundations with Python/Lesson 3/movies/entertainment_center.py
@@ -7,4 +7,13 @@ trailer_youtube_url = "https://www.youtube.com/watch?v=vwyZH85NQC4"
 
 toy_story = media.Movie(title, storyline, poster_image_url, trailer_youtube_url)
 
-print(toy_story.storyline)
+##print(toy_story.storyline)
+
+
+title = "Avatar"
+storyline = "A marine on an alien planet"
+poster_image_url = "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster."
+trailer_youtube_url = "http://www.youtube.com/watch?v=-9ceBgWV8io"
+
+avatar = media.Movie(title, storyline, poster_image_url, trailer_youtube_url)
+print(avatar.storyline)
