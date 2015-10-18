7d1307cea74ebfbee511e185f774c84ee906c6a0
William Crandell <william@crandell.ws>
2015-10-17 22:56:47 -0400

    Using more recent movies that I have enjoyed
      At least the ones that have already been released I enjoyed
      I added movies I would like to watch that have yet to be released

--- Programming Foundations with Python/Lesson 3/movies/entertainment_center.py
+++ Programming Foundations with Python/Lesson 3/movies/entertainment_center.py
@@ -3,31 +3,44 @@ from ud036_StarterCode import fresh_tomatoes
 
 ## using a dictionary of dictionaries {} = dict()
 movie_dictionary = {
-    "toy_story":{
-        "title":"Toy Story",
-        "storyline":"A story of a boy and his toys that come to life",
-        "poster_image_url":"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-        "trailer_youtube_url":"https://youtu.be/vwyZH85NQC4"
+    "1_divergent":{
+        "title":"Divergent",
+        "storyline":"A thrilling action-adventure film set in a world where people are divided into distinct factions based on human virtues.",
+        "poster_image_url":"https://upload.wikimedia.org/wikipedia/en/d/d4/Divergent.jpg",
+        "trailer_youtube_url":"https://youtu.be/336qJITnDi0"
+    },
+    "2_insurgent":{
+        "title":"The Divergent Series: Insurgent",
+        "storyline":"Insurgent raises the stakes for Tris as she searches for allies and answers in the ruins of a futuristic Chicago.",
+        "poster_image_url":"https://upload.wikimedia.org/wikipedia/en/a/af/Insurgent_poster.jpg",
+        "trailer_youtube_url":"https://youtu.be/sX9-l0iO5w4"
         },
-    "avatar":{
-        "title": "Avatar",
-        "storyline": "A marine on an alien planet",
-        "poster_image_url": "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+    "3_allegiant":{
+        "title": 'The Divergent Series: Allegiant',
+        "storyline": "After the earth-shattering revelations of INSURGENT, Tris must escape with Four and go beyond the wall enclosing Chicago.",
+        "poster_image_url": "https://upload.wikimedia.org/wikipedia/en/f/f8/Allegiantfilmposter.jpg",
         "trailer_youtube_url": "https://youtu.be/5PSNL1qE6VY"
         },
-    "the_lion_gaurd":{
-        "title": "The Lion Guard: Return of the Roar",
-        "storyline": "Meet Kion, son of Simba and Nala, and his best friend Bunga in this sneak peek from The Lion Guard: Return of the Roar, a television movie event premiering in November on Disney Channel!",
-        "poster_image_url": "http://cdn03.cdn.justjaredjr.com/wp-content/uploads/headlines/2015/10/disney-dates-lion-guard-movie-key-art.jpg",
-        "trailer_youtube_url": "https://youtu.be/2toXxKBg-kk"
+    "4_ascendant":{
+        "title": "The Divergent Series: Ascendant",
+        "storyline": "After Allegiant is Ascendant which is to arrive in 2017",
+        "poster_image_url": "http://www.thedivergentseries.movie/ascendant/assets/img/poster/poster.jpg",
+        "trailer_youtube_url": "https://youtu.be/6DWqWGwIATQ"
+        },
+    "5_star_trek":{
+        "title": "Star Trek",
+        "storyline": "The greatest adventure of all time begins with Star Trek, the incredible story of a young crew's maiden voyage onboard the most advanced starship ever created: the U.S.S. Enterprise.",
+        "poster_image_url": "https://upload.wikimedia.org/wikipedia/en/2/29/Startrekposter.jpg",
+        "trailer_youtube_url": "https://youtu.be/iGAHnZ555nI"
+        },
+    "6_star_trek_into_darkness":{
+        "title": "Star Trek Into Darkness",
+        "storyline": "When a ruthless mastermind, Khan (Benedict Cumberbatch,) declares a one-man war on the Federation, Captain Kirk (Chris Pine), Spock (Zachary Quinto), and the crew of the U.S.S. Enterprise set out on their most explosive manhunt of all time.",
+        "poster_image_url": "https://upload.wikimedia.org/wikipedia/en/5/50/StarTrekIntoDarkness_FinalUSPoster.jpg",
+        "trailer_youtube_url": "https://youtu.be/sJNyGFqgyag"
         }
     }
-
-
-the_lion_gaurd = media.Movie(movie_dictionary["the_lion_gaurd"])
-toy_story = media.Movie(movie_dictionary["toy_story"])
-avatar = media.Movie(movie_dictionary["avatar"])
-
-movies = [avatar, toy_story, the_lion_gaurd, avatar, toy_story, the_lion_gaurd]
-
+movies = []
+for movie_name in sorted(movie_dictionary.keys()):
+    movies.append(media.Movie(movie_dictionary[movie_name]))
 fresh_tomatoes.open_movies_page(movies)
--- Programming Foundations with Python/Lesson 3/movies/media.py
+++ Programming Foundations with Python/Lesson 3/movies/media.py
@@ -9,17 +9,4 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-
-
-
-#### The Way it was shown
-##
-##
-##class Movie():
-##    def __init__(self, title_, storyline_, poster_image_url_, trailer_youtube_url_):
-##        self.title = title_
-##        self.storyline = storyline_
-##        self.poster_image_url = poster_image_url_
-##        self.trailer_youtube_url = trailer_youtube_url_
-##
-###### What I did is use dict() ie dictionary for reference
+        
